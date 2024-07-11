d9924165dc4d6ebd2bdd786835e7f042549ac35a
JelleD <jelledriessen2003@gmail.com>
2024-07-11 13:05:00 +0200

    Added typings

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+declare module '@thebeyondgroup/shopify-rich-text-renderer' {
+  /** Optional options for rendering Shopify rich text to HTML */
+  export interface RichTextRendererOptions {
+    /** allows for the rich text HTML to be easily styled */
+    scoped: boolean;
+    /** convert new line character to <br/> */
+    newLineToBreak: boolean;
+    classes: {
+      /** paragraph classes */
+      p: string,
+      /** heading1 classes */
+      h1: string,
+      /** heading2 classes */
+      h2: string,
+      /** heading3 classes */
+      h3: string,
+      /** heading4 classes */
+      h4: string,
+      /** heading5 classes */
+      h5: string,
+      /** heading6 classes */
+      h6: string,
+      /** order list classes */
+      ol: string,
+      /** unordered list classes */
+      ul: string,
+      /** list item classes */
+      li: string,
+      /** anchor/link classes */
+      a: string,
+      /** bold/strong classes */
+      strong: string,
+      /** italic/em classes */
+      em: string,
+    }
+  }
+
+  /** Converts Shopify rich text to HTML */
+  export function convertSchemaToHtml(schema: string, options: RichTextRendererOptions = {}): string;
+}
