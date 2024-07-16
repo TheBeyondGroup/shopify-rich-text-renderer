cc8e7a184abea3e06739816368c510495bb05ac7
JelleD <jelledriessen2003@gmail.com>
2024-07-16 09:49:40 +0200

    Added schema input class and refactored, changes by @mcqua007

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,55 @@
 declare module '@thebeyondgroup/shopify-rich-text-renderer' {
+
+  /** Shopify rich text schema */
+  export interface Schema {
+    type: string
+    children?: Schema[]
+    level?: number
+    listType?: 'ordered' | 'unordered'
+    url?: string
+    title?: string
+    target?: string
+    bold?: boolean
+    italic?: boolean
+    value?: string
+  }
+
   /** Optional options for rendering Shopify rich text to HTML */
-  export interface RichTextRendererOptions {
+  export interface Options {
     /** allows for the rich text HTML to be easily styled */
-    scoped: boolean;
+    scoped?: boolean | string
     /** convert new line character to <br/> */
-    newLineToBreak: boolean;
-    classes: {
+    newLineToBreak?: boolean
+    classes?: {
       /** paragraph classes */
-      p: string,
+      p?: string
       /** heading1 classes */
-      h1: string,
+      h1?: string
       /** heading2 classes */
-      h2: string,
+      h2?: string
       /** heading3 classes */
-      h3: string,
+      h3?: string
       /** heading4 classes */
-      h4: string,
+      h4?: string
       /** heading5 classes */
-      h5: string,
+      h5?: string
       /** heading6 classes */
-      h6: string,
+      h6?: string
       /** order list classes */
-      ol: string,
+      ol?: string
       /** unordered list classes */
-      ul: string,
+      ul?: string
       /** list item classes */
-      li: string,
+      li?: string
       /** anchor/link classes */
-      a: string,
+      a?: string
       /** bold/strong classes */
-      strong: string,
+      strong?: string
       /** italic/em classes */
-      em: string,
+      em?: string
     }
   }
 
   /** Converts Shopify rich text to HTML */
-  export function convertSchemaToHtml(schema: string, options: RichTextRendererOptions = {}): string;
+  export function convertSchemaToHtml(schema: string | Schema | Schema[], options?: Options | string | boolean): string
 }
